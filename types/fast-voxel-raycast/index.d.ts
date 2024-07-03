1cc6f81939524dd1de4307243bfcae15d4b92360
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-07-03 01:22:05 -0700

    Fast-Voxel-Raycast Types
    Added JSDoc type annotations to the node_modules installation itself, then copied that edited content into the TS playground, and got out the `.d.ts` type equivalents from that compiled code.
    
    https://www.npmjs.com/package/fast-voxel-raycast
    
    ```js
    // @ts-check
    
    "use strict"
    
    /**
     * @param {(x: number, y: number, z: number) => boolean} getVoxel
     * @param {number} px
     * @param {number} py
     * @param {number} pz
     * @param {number} dx
     * @param {number} dy
     * @param {number} dz
     * @param {number} max_d
     * @param {[number, number, number]} hit_pos
     * @param {[number, number, number]} hit_norm
     * @returns {true | 0}
     */
    function traceRay_impl( getVoxel,
    	px, py, pz,
    	dx, dy, dz,
    	max_d, hit_pos, hit_norm) {
    
    	// consider raycast vector to be parametrized by t
    	//   vec = [px,py,pz] + t * [dx,dy,dz]
    
    	// algo below is as described by this paper:
    	// http://www.cse.chalmers.se/edu/year/2010/course/TDA361/grid.pdf
    
    	var t = 0.0
    		, floor = Math.floor
    		, ix = floor(px) | 0
    		, iy = floor(py) | 0
    		, iz = floor(pz) | 0
    
    		, stepx = (dx > 0) ? 1 : -1
    		, stepy = (dy > 0) ? 1 : -1
    		, stepz = (dz > 0) ? 1 : -1
    
    	// dx,dy,dz are already normalized
    		, txDelta = Math.abs(1 / dx)
    		, tyDelta = Math.abs(1 / dy)
    		, tzDelta = Math.abs(1 / dz)
    
    		, xdist = (stepx > 0) ? (ix + 1 - px) : (px - ix)
    		, ydist = (stepy > 0) ? (iy + 1 - py) : (py - iy)
    		, zdist = (stepz > 0) ? (iz + 1 - pz) : (pz - iz)
    
    	// location of nearest voxel boundary, in units of t
    		, txMax = (txDelta < Infinity) ? txDelta * xdist : Infinity
    		, tyMax = (tyDelta < Infinity) ? tyDelta * ydist : Infinity
    		, tzMax = (tzDelta < Infinity) ? tzDelta * zdist : Infinity
    
    		, steppedIndex = -1
    
    	// main loop along raycast vector
    	while (t <= max_d) {
    
    		// exit check
    		var b = getVoxel(ix, iy, iz)
    		if (b) {
    			if (hit_pos) {
    				hit_pos[0] = px + t * dx
    				hit_pos[1] = py + t * dy
    				hit_pos[2] = pz + t * dz
    			}
    			if (hit_norm) {
    				hit_norm[0] = hit_norm[1] = hit_norm[2] = 0
    				if (steppedIndex === 0) hit_norm[0] = -stepx
    				if (steppedIndex === 1) hit_norm[1] = -stepy
    				if (steppedIndex === 2) hit_norm[2] = -stepz
    			}
    			return b
    		}
    
    		// advance t to next nearest voxel boundary
    		if (txMax < tyMax) {
    			if (txMax < tzMax) {
    				ix += stepx
    				t = txMax
    				txMax += txDelta
    				steppedIndex = 0
    			} else {
    				iz += stepz
    				t = tzMax
    				tzMax += tzDelta
    				steppedIndex = 2
    			}
    		} else {
    			if (tyMax < tzMax) {
    				iy += stepy
    				t = tyMax
    				tyMax += tyDelta
    				steppedIndex = 1
    			} else {
    				iz += stepz
    				t = tzMax
    				tzMax += tzDelta
    				steppedIndex = 2
    			}
    		}
    
    	}
    
    	// no voxel hit found
    	if (hit_pos) {
    		hit_pos[0] = px + t * dx
    		hit_pos[1] = py + t * dy
    		hit_pos[2] = pz + t * dz
    	}
    	if (hit_norm) {
    		hit_norm[0] = hit_norm[1] = hit_norm[2] = 0
    	}
    
    	return 0
    
    }
    
    // conform inputs
    
    /**
     * @param {(x: number, y: number, z: number) => boolean} getVoxel
     * @param {[number, number, number]} origin
     * @param {[number, number, number]} direction
     * @param {number} max_d
     * @param {[number, number, number]} hit_pos
     * @param {[number, number, number]} hit_norm
     * @returns {true | 0}
     */
    function traceRay(getVoxel, origin, direction, max_d, hit_pos, hit_norm) {
    	var px = +origin[0]
    		, py = +origin[1]
    		, pz = +origin[2]
    		, dx = +direction[0]
    		, dy = +direction[1]
    		, dz = +direction[2]
    		, ds = Math.sqrt(dx * dx + dy * dy + dz * dz)
    
    	if (ds === 0) {
    		throw new Error("Can't raycast along a zero vector")
    	}
    
    	dx /= ds
    	dy /= ds
    	dz /= ds
    	if (typeof (max_d) === "undefined") {
    		max_d = 64.0
    	} else {
    		max_d = +max_d
    	}
    	return traceRay_impl(getVoxel, px, py, pz, dx, dy, dz, max_d, hit_pos, hit_norm)
    }
    
    module.exports = traceRay
    ```

---
+++ types/fast-voxel-raycast/index.d.ts
@@ -0,0 +1,4 @@
+declare module "fast-voxel-raycast" {
+  export = traceRay;
+  declare function traceRay(getVoxel: (x: number, y: number, z: number) => boolean, origin: [number, number, number], direction: [number, number, number], max_d: number, hit_pos: [number, number, number], hit_norm: [number, number, number]): true | 0;
+}
